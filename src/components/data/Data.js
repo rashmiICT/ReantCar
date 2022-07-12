7ee409e15795d96fe8c979f220e6378e9e4d7878
Rashmi Pituwala <83907599+rashmiICT@users.noreply.github.com>
2022-07-12 22:13:48 +0530

    Adding data

---
+++ src/components/data/Data.js
@@ -0,0 +1,346 @@
+export const nav = [
+    {
+      text: "home",
+      path: "/",
+    },
+    {
+      text: "about",
+      path: "/about",
+    },
+    {
+      text: "services",
+      path: "/services",
+    },
+    {
+      text: "blog",
+      path: "/blog",
+    },
+    {
+      text: "Booking",
+      path: "/Booking",
+    },
+    {
+      text: "contact",
+      path: "/contact",
+    },
+  ]
+  export const featured = [
+    {
+      cover: "../images/hero/h1.png",
+      name: "Rent a Car",
+      total: "122 Property",
+    },
+    {
+      cover: "../images/hero/h2.png",
+      name: "Charging ",
+      total: "155 Property",
+    },
+    {
+      cover: "../images/hero/h3.png",
+      name: "Services",
+      total: "300 Property",
+    },
+    {
+      cover: "../images/hero/h4.png",
+      name: "24 hours work",
+      total: "80 Property",
+    },
+    {
+      cover: "../images/hero/h6.png",
+      name: " Free Fuel",
+      total: "80 Property",
+    },
+  ]
+  export const list = [
+    {
+      id: 1,
+      cover: "../images/list/p-1.jpg",
+      name: "Adui A4",
+      location: "210 new road,colombo",
+      category: "For Rent",
+      price: "$3000",
+      type: "Apartment",
+    },
+    {
+      id: 2,
+      cover: "../images/list/p-2.jpg",
+      name: "BMW 2018",
+      location: "5698 Zirak Road, Middeniya",
+      category: "For Rent ",
+      price: "$9750",
+      type: "Condos",
+    },
+    {
+      id: 3,
+      cover: "../images/list/p-3.jpg",
+      name: "Honda Civic",
+      location: "5624 Mooker Market, Mirihana",
+      category: "For Rent",
+      price: "$860",
+      type: "Offices",
+    },
+    {
+      id: 4,
+      cover: "../images/list/p-4.jpg",
+      name: "Toyota Premio 2021",
+      location: "5621 Liverpool, Delgoda",
+      category: "For Rent",
+      price: "$7,540",
+      type: "Homes & Villas",
+    },
+    {
+      id: 5,
+      cover: "../images/list/p-5.jpg",
+      name: "Toyota Priouse",
+      location: "210 Montreal Road, Dehiwala",
+      category: "For Rent",
+      price: "$4850",
+      type: "Commercial",
+    },
+    {
+      id: 6,
+      cover: "../images/list/p-6.jpg",
+      name: "Benz 2022",
+      location: "210 Zirak Road, Kottawa",
+      category: "For Rent",
+      price: "$2,742",
+      type: "Apartment",
+    },
+    {
+      id: 7,
+      cover: "../images/list/p-7.jpg",
+      name: "Benz 2022",
+      location: "210 Zirak Road, Kottawa",
+      category: "For Rent",
+      price: "$2,742",
+      type: "Apartment",
+    },
+    {
+      id: 8,
+      cover: "../images/list/p-8.jpg",
+      name: "Benz 2022",
+      location: "210 Zirak Road, Kottawa",
+      category: "For Rent",
+      price: "$2,742",
+      type: "Apartment",
+    },
+    {
+      id: 9,
+      cover: "../images/list/p-9.jpg",
+      name: "Benz 2022",
+      location: "210 Zirak Road, Kottawa",
+      category: "For Rent",
+      price: "$2,742",
+      type: "Apartment",
+    },
+  ]
+  export const awards = [
+    {
+      icon: <i class='fa-solid fa-trophy'></i>,
+      num: "32 M	",
+      name: "Blue Burmin Award",
+    },
+    {
+      icon: <i class='fa-solid fa-briefcase'></i>,
+      num: "43 M",
+      name: "Mimo X11 Award",
+    },
+    {
+      icon: <i class='fa-solid fa-lightbulb'></i>,
+      num: "51 M",
+      name: "Australian UGC Award",
+    },
+    {
+      icon: <i class='fa-solid fa-heart'></i>,
+      num: "42 M",
+      name: "IITCA Green Award",
+    },
+  ]
+  export const location = [
+    {
+      id: 1,
+      name: "New Orleans, Louisiana",
+      Villas: "12 Villas",
+      Apartments: "10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-1.png",
+    },
+    {
+      id: 2,
+      name: "Jerrsy, United State",
+      Villas: "12 Villas",
+      Apartments: "10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-2.png",
+    },
+    {
+      id: 3,
+      name: "Liverpool, London",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-3.png",
+    },
+    {
+      id: 4,
+      name: "NewYork, United States",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-4.png",
+    },
+    {
+      id: 5,
+      name: "Montreal, Canada",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-5.png",
+    },
+    {
+      id: 6,
+      name: "California, USA",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-6.png",
+    },
+  ]
+  export const team = [
+    {
+      list: "50",
+      cover: "../images/customer/team-1.jpg",
+      address: "Anuradhapura,Sri Lanka",
+      name: "Rashmi Pituwala",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "70",
+      cover: "../images/customer/team-2.jpg",
+      address: "Badulla ,Sri Lanka",
+      name: "Ishara Deshapriya",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "80",
+      cover: "../images/customer/team-3.jpg",
+      address: "Kottawa,Colombo",
+      name: "Shenuri Fernando",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "51",
+      cover: "../images/customer/team-4.jpg",
+      address: "Gampaha ,Sri Lanka",
+      name: "Akeshi Warnakulasooriya",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "42",
+      cover: "../images/customer/team-5.jpg",
+      address: "Kaluthra , Sri Lanka",
+      name: "Mahesh abdul",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "38",
+      cover: "../images/customer/team-6.jpg",
+      address: "Dehiwala,Sri Lanka",
+      name: "shawi eranga",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+  ]
+  export const price = [
+    {
+      plan: "Basic",
+      price: "30",
+      ptext: "per user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "99.5% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "120GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "5GB Cloud Storage",
+        },
+        { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+        { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+      ],
+    },
+    {
+      best: "Best Value",
+      plan: "Standard",
+      price: "50",
+      ptext: "per user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "99.5% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "150GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "10GB Cloud Storage",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Personal Help Support",
+        },
+        {
+          change: "color",
+          icon: <i class='fa-solid fa-x'></i>,
+          text: "Enterprise SLA",
+        },
+      ],
+    },
+    {
+      plan: "Platinum",
+      price: "79",
+      ptext: "2 user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "100% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "200GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "20GB Cloud Storage",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Personal Help Support",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Enterprise SLA",
+        },
+      ],
+    },
+  ]
+  export const footer = [
+    {
+      title: "LAYOUTS",
+      text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+    },
+    {
+      title: "ALL SECTIONS",
+      text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+    },
+    {
+      title: "COMPANY",
+      text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+    },
+  ]
+  
