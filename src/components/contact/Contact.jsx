661de5e29e521a4b50af577f1f8c20c2fafd992e
Rashmi Pituwala <83907599+rashmiICT@users.noreply.github.com>
2022-07-12 22:12:54 +0530

    Adding contact page

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Back from '../common/Back'
+import img from "../images/pricing1.jpg"
+import "./contact.css"
+
+const Contact = () => {
+  return (
+    <>
+      <section className='contact mb'>
+        <Back name='Contact Us' title='Get Helps & Friendly Support' cover={img} />
+        <div className='container'>
+          <form className='shadow'>
+            <h2><center>Fillup The Form</center></h2> <br />
+            <div>
+              <input type='text' placeholder='Name' />
+              <input type='text' placeholder='Email' />
+            </div>
+            <input type='text' placeholder='Subject' />
+            <textarea cols='30' rows='10'></textarea>
+            <button>Submit Request</button>
+          </form>
+        </div>
+      </section>
+    </>
+  ) 
+}
+
+export default Contact
